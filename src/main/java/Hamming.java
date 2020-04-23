e2674f3ce493cff7aedcf5e8a9da7ac175df83a5
Roosevelt <42326422+Docrozay@users.noreply.github.com>
2020-04-23 13:14:52 +0100

    Initial commit

---
+++ src/main/java/Hamming.java
@@ -0,0 +1,21 @@
+
+
+class Hamming {
+
+
+    private int result = 0;
+
+    Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
+        // complete the constructor
+
+    }
+
+
+    int getHammingDistance() {
+
+        return result;
+
+    }
+
+
+}
